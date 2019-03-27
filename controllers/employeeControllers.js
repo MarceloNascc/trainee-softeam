11e6bf7a99edeb8e4a46a117477e0e56a97edaad
João Marcelo <joao.marcelo@dcomp.ufs.br>
2019-03-27 02:42:25 -0300

    resolvendo bugs do CRUD

--- controllers/employeeControllers.js
+++ controllers/employeeControllers.js
@@ -1,9 +1,10 @@
-const employee = require('../models/employeeModel');
+const Employee = require('../models/employeeModel');
 
 //funcionalidade: retornar todos os funcionários
 const readAll = async (req, res) => {
     try {
         const employees = await Employee.find({});
+
         return res.status(200).json({ message: 'Todos os funcionários:', employees });
     } catch (error) {
         return res.status(500).json({message: 'Erro ao tentar mostrar todos os funcionários:', error});
@@ -12,15 +13,15 @@ const readAll = async (req, res) => {
 
 //funcionalidade: retornar um funcionário pelo cpf
 const readOne = async (req, res) => {
+    const { cpf } = req.params;
     try {
-        const { cpf } = req.params;
-        const employee = await Employee.findOne({ cpf: cpf });
+        const employee = await Employee.findOne({ cpf: parseInt(cpf) });
 
-        if(!employee){
+        if(employee === null){
             return res.status(404).json({message: `Funcionário com cpf = ${cpf} não encontrado`});
         }
-
-        return res.status(200).json({ message: 'Funcionário:', employees });
+        
+        return res.status(200).json({ message: 'Funcionário:', employee });
     } catch (error) {
         return res.status(500).json({message: `Erro ao tentar mostrar o funcionário com cpf = ${cpf}:`, error});
     }
@@ -28,28 +29,28 @@ const readOne = async (req, res) => {
 
 //funcionalidade: atualizar um funcionário
 const update = async (req, res) => {
+    const { cpf: cpfParam } = req.params;
     try {
-        const { cpf } = req.params;
         const { name: newName, cpf, phone: newPhone, code: newCode, number: newNumber, complement: newComplement } = req.body;
         const updateEmployee = await Employee.findOneAndUpdate(
             {
-                cpf: cpf
+                cpf: cpfParam
             }, 
             {
                 name: newName,
                 cpf,
                 phone: newPhone,
                 code: newCode,
-                number: newNumber,
+                number: parseInt(newNumber),
                 complement: newComplement
             }
         );
 
-        if(!updateEmployee){
+        if(updateEmployee === null){
             return res.status(404).json({message: `Funcionário com cpf = ${cpf} não encontrado`});
         }
 
-        return res.status(200).json({ message: 'Funcionário atualizado!\nDados atuais:', employees });
+        return res.status(200).json({ message: 'Funcionário atualizado!', updateEmployee });
     } catch (error) {
         return res.status(500).json({message: `Erro ao tentar atualizar o funcionário com cpf = ${cpf}:`, error});
     }
@@ -57,15 +58,15 @@ const update = async (req, res) => {
 
 //funcionalidade: remover um funcionário
 const delet = async (req, res) => {
+    const { cpf } = req.params;
     try {
-        const { cpf } = req.params;
         const employee = await Employee.findOneAndDelete({ cpf: cpf });
     
         if(!employee){
             return res.status(404).json({message: `Funcionário com cpf = ${cpf} não encontrado`});
         }
         
-        return res.status(200).json({ message: `Funcionário removido:`, employees });
+        return res.status(200).json({ message: `Funcionário removido:`, employee });
     } catch (error) {
         return res.status(500).json({message: `Erro ao tentar remover o funcionário com cpf = ${cpf}:`, error});
     }
@@ -75,16 +76,16 @@ const delet = async (req, res) => {
 const create = async (req, res) => {
     try {
         const {name, cpf, phone, code, number, complement} = req.body;
-
+        
         const employee = new Employee ({
             name,
             cpf,
             phone,
             code,
-            number,
+            number: parseInt(number),
             complement
         });
-
+        
         await employee.save();
         return res.status(201).json({ message: 'Funcionário adicionado:', employee });
     } catch (error) {
