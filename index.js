e085803bc31fd4fb4e67d8ddd87dbb7c7c204be9
João Marcelo <joao.marcelo@dcomp.ufs.br>
2019-03-30 23:09:47 -0300

    documentação adicionada

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express'); //controle de rotas
 const bodyParser = require('body-parser'); //converte JSON
 const mongoose = require('mongoose'); //gerenciamento do banco de dados
 const employeeRouter = require('./router/employeeRouter');
+const swagger = require('swagger-ui-express'); //documentação
+const swaggerDoc = require('./doc/doc.json');
 
 mongoose.Promise = global.Promise; //erros com promise
 mongoose.connect('mongodb://localhost:27017/traineeSofteam'); //conectando com o db
@@ -11,11 +13,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); //permite valores além de strings
 
-
-//criando um middleware para teste da aplicação
+//criando uma rota de teste da aplicação
 app.use('/teste', (req, res) => res.json('A aplicação está rodando :)'));
 app.use('/employee', employeeRouter());
-
+app.use('/', swagger.serve, swagger.setup(swaggerDoc));
 
 //colocando a aplicação para rodar
 module.exports = app.listen(3000, () => console.log('RUN'));
